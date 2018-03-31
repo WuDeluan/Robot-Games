e23572a5ba1dbfbdce552eff5827b7e04d045088
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2018-03-31 18:51:44 +0800

    Update Gobang.cpp

--- Gobang.cpp
+++ Gobang.cpp
@@ -1,36 +1,10 @@
 #include "Gobang_Base.h"
+#include "Gobang_ManToMan.h"
+#include "Gobang_ManToCom.h"
 
 int main()
 {
-	char x, y;
-	Gobang G = Gobang();
-	G.Print_Checkerboard();
-	while (1)
-	{
-		while(true){
-			cout << "请黑方落子：";
-			cin >> x;
-			cin >> y; 
-			if (G.setBoard(x, y, BLACK) == -1)
-				cout << "请选择正确的位置落子！" << endl;
-			else
-				break;
-		} 
-		G.Print_Checkerboard();
-		if (G.IsWin(x, y) == 1)
-			cout << "黑方获胜!" << endl;
-		while (true) {
-			cout << "请白方落子：";
-			cin >> x;
-			cin >> y;
-			if (G.setBoard(x, y, WHITE) == -1)
-				cout << "请选择正确的位置落子！" << endl;
-			else
-				break;
-		}
-		G.Print_Checkerboard();
-		if (G.IsWin(x, y) == 1)
-			cout << "白方获胜!" << endl;
-	}
-
+	ManToMan Gobang1 =  ManToMan();
+	Gobang1.Print_Menu();
+	ManToCom Gobang2 = ManToCom();
 }
