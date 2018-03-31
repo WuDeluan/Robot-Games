95ecd80687e693929425bb00ac51e7a73a84e69b
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2018-03-31 18:50:09 +0800

    Update Gobang_Base.h

--- Gobang_Base.h
+++ Gobang_Base.h
@@ -1,8 +1,29 @@
 #ifndef GOBANG_BASE_H
 #define GOBANG_BASE_H
 
-#include "Gobang_Class.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
 
+#define EMPTY  0 //空子
+#define BLACK  1 //黑子
+#define WHITE  2 //白子
+int Direct[4][2] = { { 1,-1 },{ 1,0 },{ 1,1 },{ 0,1 } };
+
+class Gobang
+{
+private:
+	int Board[15][15]; //棋盘
+public:
+	Gobang();
+	void Init_Board(); //初始化棋盘
+	void Print_Checkerboard(); //打印棋盘
+	int setBoard(int &x, char &y, int piece); //落子
+	int getBoard(int x, int y);
+};
 Gobang::Gobang()
 {
 	Init_Board();
@@ -11,21 +32,21 @@ Gobang::Gobang()
 void Gobang::Init_Board()
 {
 	int i, j;
-	for (i = 0; i < 19; i++)
-		for (j = 0; j < 19; j++)
-			Board[i][j] = EMPTY; 
+	for (i = 0; i < 15; i++)
+		for (j = 0; j < 15; j++)
+			Board[i][j] = EMPTY;
 }
 
 void Gobang::Print_Checkerboard()
 {
 	int i, j;
 	//system("cls");
-	cout << "----------五子棋人人对弈程序----------\n" << endl;
-	cout << "  A B C D E F G H I J K L M N O P Q R S" << endl;
-	for (i = 0; i < 19; i++)
+	//cout << "----------五子棋人人对弈程序----------\n" << endl;
+	cout << "  A B C D E F G H I J K L M N O" << endl;
+	for (i = 0; i < 15; i++)
 	{
 		cout << setw(2) << i + 1;
-		for (j = 0; j < 19; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (Board[i][j] == BLACK)
 				cout << "○";
@@ -37,16 +58,16 @@ void Gobang::Print_Checkerboard()
 				{
 					if (j == 0)
 						cout << "┌ ";
-					else if (j == 18)
+					else if (j == 14)
 						cout << "┐ ";
-					else 
+					else
 						cout << "┬ ";
 				}
-				else if (i == 18)
+				else if (i == 14)
 				{
 					if (j == 0)
 						cout << "└ ";
-					else if (j == 18)
+					else if (j == 14)
 						cout << "┘ ";
 					else
 						cout << "┴ ";
@@ -55,7 +76,7 @@ void Gobang::Print_Checkerboard()
 				{
 					if (j == 0)
 						cout << "├ ";
-					else if (j == 18)
+					else if (j == 14)
 						cout << "┤ ";
 					else
 						cout << "┼ ";
@@ -66,11 +87,11 @@ void Gobang::Print_Checkerboard()
 	}
 }
 
-int Gobang::setBoard(char &x, char &y, int Piece_Color)
+int Gobang::setBoard(int &x, char &y, int Piece_Color)
 {
-	x -= '1';
+	x -= 1;
 	y -= 'A';
-	if (x >= 0 && x < 19 && y >= 0 && y < 19 && Board[x][y] == EMPTY) //在棋盘内，且为空
+	if (x >= 0 && x < 15 && y >= 0 && y < 15 && Board[x][y] == EMPTY) //在棋盘内，且为空
 	{
 		Board[x][y] = Piece_Color;
 		return 0;
@@ -79,32 +100,9 @@ int Gobang::setBoard(char &x, char &y, int Piece_Color)
 		return -1;
 }
 
-int Direct[4][2] = { { 1,-1 },{ 1,0 },{ 1,1 },{ 0,1 } };
-int Gobang::IsWin(int x, int y)
+int Gobang::getBoard(int x, int y)
 {
-	int i, tx, ty, len;
-	int side = Board[x][y];//取得落子颜色
-	for (i = 0; i < 4; i++)//4个方向的循环
-	{
-		tx = x; ty = y;//设置观察起点
-		len = 0;//设置计数器
-		while (tx >= 0 && tx < 19 && ty >= 0 && ty < 19 && Board[tx][ty] == side)//连珠条件
-		{
-			tx -= Direct[i][0];//沿一个方向
-			ty -= Direct[i][1];//移动观察点
-		}
-		tx += Direct[i][0];
-		ty += Direct[i][1];
-		while (tx >= 0 && tx < 19 && ty >= 0 && ty < 19 && Board[tx][ty] == side)//连珠条件
-		{
-			len++;//计数
-			tx += Direct[i][0];//沿相反方向
-			ty += Direct[i][1];//移动观察点
-		}
-		if (len >= 5)//比较连珠数目
-			return 0;//获胜
-	}
-	return -1;//为获胜
+	return Board[x][y];
 }
 
 #endif
