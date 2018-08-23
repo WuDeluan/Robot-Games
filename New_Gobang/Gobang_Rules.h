c6534c2688eb36d9b4b5e64b5f493d140cd65f5c
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2018-08-23 22:30:58 +0800

    Update Gobang_Rules.h

--- New_Gobang/Gobang_Rules.h
+++ New_Gobang/Gobang_Rules.h
@@ -14,11 +14,13 @@ public:
 	int IsWin(int x, int y); //判断某一方是否连成五子
 	int PreJudge(int &x, int &y); //预判是否出现必须防守的情况
 	int Eval(int x, int y);
+	int Eveluate();
+	int Analysis(int x, int y);
 	int Function(int len, string side1, string side2);
 	int minmaxSearch(int depth, int &tx, int &ty);
 	int MaxSearch(int depth, int tx, int ty);
 	int MinSearch(int depth, int tx, int ty);
-	int Alphabeta(int &tx, int &ty,int depth, int a, int b,int player);
+	int Alphabeta(int &tx, int &ty, int depth, int a, int b, int player);
 };
 
 //指定开局
@@ -295,6 +297,264 @@ int Gobang_Rules::Eval(int x, int y)
 	return bestValue;
 }
 
+int Gobang_Rules::Eveluate()
+{
+	int x, y, k;
+	int Side;
+	memset(TypeRecord, TOBEANALSIS, 15 * 15 * 4 * 4);
+	memset(TypeCount, 0, 40 * 4);
+	for (x = 0; x < 15; x++)
+		for (y = 0; y < 15; y++)
+			if (Gobang::getBoard(x, y) != EMPTY)
+				Analysis(x, y);
+
+	for (x = 0; x < 15; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			Side = Gobang::getBoard(x, y);
+			if (Side != EMPTY)
+			{
+				for (k = 0; k < 4; k++)
+				{
+					switch (TypeRecord[x][y][k])
+					{
+					case FIVE://五连
+						TypeCount[Side][FIVE]++; break;
+					case FOUR://活四
+						TypeCount[Side][FOUR]++; break;
+					case FFOUR://冲四
+						TypeCount[Side][FFOUR]++; break;
+					case DFOUR://死四
+						TypeCount[Side][DFOUR]++; break;
+					case THREE://活三
+						TypeCount[Side][THREE]++; break;
+					case STHREE://眠三
+						TypeCount[Side][STHREE]++; break;
+					case DTHREE://眠三
+						TypeCount[Side][DTHREE]++; break;
+					case TWO://活二
+						TypeCount[Side][TWO]++; break;
+					case STWO://眠二
+						TypeCount[Side][STWO]++; break;
+					default:break;
+					}
+				}
+			}
+		}
+	}
+
+	//如果已五连,返回极值
+	if (TypeCount[COM][FIVE])
+		return 9999;
+	else if (TypeCount[MAN][FIVE])
+		return -9999;
+
+	//两个冲四等于一个活四
+	if (TypeCount[COM][FFOUR] > 1)
+		TypeCount[COM][FOUR]++;
+	if (TypeCount[MAN][FFOUR] > 1)
+		TypeCount[MAN][FOUR]++;
+
+	int CValue = 0, MValue = 0;
+
+	if (TypeCount[COM][FOUR])
+		return 9990;//活四,白胜返回极值
+	if (TypeCount[COM][FFOUR])
+		return 9980;//冲四,白胜返回极值
+	if (TypeCount[MAN][FOUR])
+		return -9970;//白无冲四活四,而黑有活四,黑胜返回极值
+	if (TypeCount[MAN][FFOUR] && TypeCount[MAN][THREE])
+		return -9960;//而黑有冲四和活三,黑胜返回极值
+	if (TypeCount[COM][THREE] && TypeCount[MAN][FFOUR] == 0)
+		return 9950;//白有活三而黑没有四,白胜返回极值
+	if (TypeCount[MAN][THREE] > 1 && TypeCount[WHITE][FFOUR] == 0 && TypeCount[COM][THREE] == 0 && TypeCount[COM][STHREE] == 0)
+		return -9940;//黑的活三多于一个,而白无四和三,黑胜返回极值
+	if (TypeCount[COM][THREE] > 1)
+		CValue += 2000;//白活三多于一个,白棋价值加2000
+	else //否则白棋价值加200
+	{
+		if (TypeCount[COM][THREE])
+			CValue += 200;
+	}
+	if (TypeCount[MAN][THREE] > 1)
+		MValue += 500;//黑的活三多于一个,黑棋价值加500
+	else //否则黑棋价值加100
+	{	
+		if (TypeCount[MAN][THREE])
+			MValue += 100;
+	}
+	//每个眠三加10
+	if (TypeCount[COM][STHREE])
+		CValue += TypeCount[COM][STHREE] * 10;
+	//每个眠三加10
+	if (TypeCount[MAN][STHREE])
+		MValue += TypeCount[MAN][STHREE] * 10;
+	//每个活二加4
+	if (TypeCount[COM][TWO])
+		CValue += TypeCount[COM][TWO] * 4;
+	//每个活二加4
+	if (TypeCount[MAN][STWO])
+		MValue += TypeCount[MAN][TWO] * 4;
+	//每个眠二加1
+	if (TypeCount[COM][STWO])
+		CValue += TypeCount[COM][STWO]; 
+	//每个眠二加1
+	if (TypeCount[MAN][STWO])
+		MValue += TypeCount[MAN][STWO];
+
+	//加上所有棋子的位置价值
+	for (x = 0; x < 15; x++)
+		for (y = 0; y < 15; y++)
+		{
+			Side = Gobang::getBoard(x, y);
+			if (Side != EMPTY)
+				if (Side == COM)
+					CValue += PosValue[x][y];
+				else
+					MValue += PosValue[x][y];
+		}
+
+	//返回估值
+		return CValue - MValue;
+}
+
+int Gobang_Rules::Analysis(int x, int y)
+{
+	int i, j, len;
+	string side1, side2;
+	int Color = COM;
+	for (i = 0; i < 4; i++)
+	{
+		if (TypeRecord[x][y][i] == TOBEANALSIS)
+		{
+			len = 0;
+			side1.clear(); side2.clear();
+			while (Gobang::getBoard(x, y) == Color)
+			{
+				x -= Direct[i][0];
+				y -= Direct[i][1];
+			}
+			for (j = 0; j < 5; j++, x -= Direct[i][0], y -= Direct[i][1]) {
+				if (Gobang::getBoard(x, y) == EMPTY && x >= 0 && y >= 0)
+					side1 += '#';
+				else if (Gobang::getBoard(x, y) == Color && x >= 0 && y >= 0)
+					side1 += '0';
+				else
+					side1 += '1';
+			}
+			x += Direct[i][0] * 6;
+			y += Direct[i][1] * 6;
+			while (Gobang::getBoard(x, y) == Color && x >= 0 && y >= 0)
+			{
+				len++;
+				TypeRecord[x][y][i] = ANALSISED;
+				x += Direct[i][0];
+				y += Direct[i][1];
+			}
+			for (j = 0; j < 5; j++, x += Direct[i][0], y += Direct[i][1]) {
+				if (Gobang::getBoard(x, y) == EMPTY && x >= 0 && y >= 0)
+					side2 += '#';
+				else if (Gobang::getBoard(x, y) == Color && x >= 0 && y >= 0)
+					side2 += '0';
+				else
+					side2 += '1';
+			}
+		}
+
+		if (len >= 5)
+		{
+			//cout << "五连" << endl; 
+			TypeRecord[x][y][i] = FIVE;
+		}
+		else if (len == 4)
+		{
+			if (side1[0] == '#' && side2[0] == '#')
+			{
+				//cout << "活四" << endl;
+				TypeRecord[x][y][i] = FOUR;
+			}
+			else if (side1[0] == '1' && side2[0] == '#' || side1[0] == '#' && side2[0] == '1')
+			{
+				//cout << "冲四" << endl;
+				TypeRecord[x][y][i] = FFOUR;
+			}
+			else if (side1[0] == '1' && side2[0] == '1')
+			{
+				//cout << "死四" << endl;
+				TypeRecord[x][y][i] = DFOUR;
+			}
+		}
+		else if (len == 3)
+		{
+			if (side1[0] == '1' && side2.compare(0, 2, "#0") == 0 || side1.compare(0, 2, "#0") == 0 && side2[0] == '1')
+			{
+				//cout << "冲四" << endl;
+				TypeRecord[x][y][i] = FFOUR;
+			}
+			else if (side1.compare(0, 2, "##") == 0 && side2.compare(0, 2, "##") == 0)
+			{
+				//cout << "连活三" << endl;
+				TypeRecord[x][y][i] = THREE;
+			}
+			else if (side1[0] == '1' && side2.compare(0, 2, "##") == 0 || side1.compare(0, 2, "##") == 0 && side2[0] == '1')
+			{
+				//cout << "眠三" << endl;
+				TypeRecord[x][y][i] = STHREE;
+			}
+			else if (side1.compare(0, 2, "#1") == 0 && side2.compare(0, 2, "#1") == 0)
+			{
+				//cout << "眠三" << endl; 
+				TypeRecord[x][y][i] = STHREE;
+			}
+			else if (side1[0] == '1' && side2[0] == '1')
+			{
+				//cout << "死三" << endl; 
+				TypeRecord[x][y][i] = DTHREE;
+			}
+		}
+		else if (len == 2)
+		{
+			if (side1.compare(0, 3, "#00") == 0 || side2.compare(0, 3, "#00") == 0)
+			{
+				//cout << "冲四" << endl;
+				TypeRecord[x][y][i] = FFOUR;
+			}
+			else if (side1[0] == '#' && side2.compare(0, 3, "#0#") == 0 || side1.compare(0, 3, "#0#") == 0 && side2[0] == '#')
+			{
+				//cout << "跳活三" << endl;
+				TypeRecord[x][y][i] = THREE;
+			}
+			else if (side1[0] == '1' && side2.compare(0, 3, "#1#") == 0 || side1.compare(0, 3, "#1#") == 0 && side2[0] == '1')
+			{
+				//cout << "眠三" << endl;
+				TypeRecord[x][y][i] = STHREE;
+			}
+			else if (side1[0] == '#' && side2.compare(0, 3, "#10") == 0 || side1.compare(0, 3, "#10") == 0 && side2[0] == '#')
+			{
+				//cout << "眠三" << endl;
+				TypeRecord[x][y][i] = STHREE;
+			}
+			else if (side1.compare(0, 3, "##0") == 0 || side2.compare(0, 3, "##0") == 0)
+			{
+				//cout << "眠三" << endl;
+				TypeRecord[x][y][i] = STHREE;
+			}
+			else if (side1.compare(0, 2, "##") == 0 && side2.compare(0, 2, "##") == 0)
+			{
+				//cout << "活二" << endl;
+				TypeRecord[x][y][i] = TWO;
+			}
+			else if (side1[0] == '1' && side2.compare(0, 3, "###") == 0 || side1.compare(0, 3, "###") == 0 && side2[0] == '1')
+			{
+				//cout << "眠二" << endl;
+				TypeRecord[x][y][i] = STWO;
+			}
+		}
+	}
+
+}
+
 int Gobang_Rules::Function(int len, string side1, string side2)
 {
 	int Value = 0;
@@ -390,24 +650,24 @@ int Gobang_Rules::Function(int len, string side1, string side2)
 
 int Gobang_Rules::minmaxSearch(int depth, int &tx, int &ty)
 {
-    int bestMoveX, bestMoveY;
+	int bestMoveX, bestMoveY;
 	int x, y;
 	int bestValue = -INFINITY;
 	if (depth == 0)
-	    return Eval(tx, ty);
+		return Eval(tx, ty);
 	else
 	{
 		//for (x = tx - 4; x <= tx + 4; x++)
-		for(x = 0;x < 15;x++)
+		for (x = 0; x < 15; x++)
 		{
 			//for (y = ty - 4; y <= ty + 4; y++)
-			for(y = 0;y < 15;y++)
+			for (y = 0; y < 15; y++)
 			{
 				//if (x >= 0 && x < 15 && y >= 0 && y < 15 && Gobang::getBoard(x, y) == EMPTY)
 				if (Gobang::getBoard(x, y) == EMPTY)
 				{
 
-					Gobang::setBoard(x, y, COM); 
+					Gobang::setBoard(x, y, COM);
 					if (Eval(x, y) >= +INFINITY)
 					{
 						tx = x;
@@ -443,7 +703,7 @@ int Gobang_Rules::MaxSearch(int depth, int tx, int ty)
 	if (depth == 0)
 	{
 		int L = Eval(tx, ty);
-		//cout << "max  " << L << endl;
+		cout << "max  " << L << endl;
 		return L;
 	}
 	else
@@ -459,7 +719,7 @@ int Gobang_Rules::MaxSearch(int depth, int tx, int ty)
 				if (Gobang::getBoard(x, y) == EMPTY)
 				{
 					setBoard(x, y, COM);
-					value = MinSearch(depth - 1, x, y);
+					value = MinSearch(depth - 1, tx, ty);
 					if (value > bestValue)
 						bestValue = value;
 					setEmpty(x, y);
@@ -467,17 +727,17 @@ int Gobang_Rules::MaxSearch(int depth, int tx, int ty)
 			}
 		}
 	}
-	//cout <<"                      MAX   "<< bestValue << endl;
+	cout << "                      MAX   " << bestValue << endl;
 	return bestValue;
 }
 
 int Gobang_Rules::MinSearch(int depth, int tx, int ty)
-{  
+{
 	int bestValue = INFINITY, x, y;
 	if (depth == 0)
 	{
 		int L = Eval(tx, ty);
-		//cout << "min  " << L << endl;
+		cout << "min  " << L << endl;
 		return L;
 	}
 	else
@@ -490,22 +750,22 @@ int Gobang_Rules::MinSearch(int depth, int tx, int ty)
 			for (y = 0; y < 15; y++)
 			{
 				//if (x >= 0 && x < 15 && y >= 0 && y < 15 && Gobang::getBoard(x, y) == EMPTY)
-				if(Gobang::getBoard(x, y) == EMPTY)
+				if (Gobang::getBoard(x, y) == EMPTY)
 				{
-					setBoard(x, y, MAN); 
-					value = MaxSearch(depth - 1, x, y);
+					setBoard(x, y, MAN);
+					value = MaxSearch(depth - 1, tx, ty);
 					if (value < bestValue)
 						bestValue = value;
-					setEmpty(x, y); 
+					setEmpty(x, y);
 				}
 			}
 		}
 	}
-	//cout << "                           MIN   " << bestValue << endl;
+	cout << "                           MIN   " << bestValue << endl;
 	return bestValue;
 }
 
-int Gobang_Rules::Alphabeta(int &tx, int &ty,int depth, int a, int b,int player)
+int Gobang_Rules::Alphabeta(int &tx, int &ty, int depth, int a, int b, int player)
 {
 	int x, y, value;
 	if (depth == 0)
@@ -528,8 +788,8 @@ int Gobang_Rules::Alphabeta(int &tx, int &ty,int depth, int a, int b,int player)
 					}
 					Gobang::setEmpty(x, y);
 					if (b <= a)
-						break; 
-				}	
+						break;
+				}
 			}
 		}
 		return a;
@@ -546,8 +806,8 @@ int Gobang_Rules::Alphabeta(int &tx, int &ty,int depth, int a, int b,int player)
 					b = Min(b, Alphabeta(x, y, depth - 1, a, b, COM));
 					Gobang::setEmpty(x, y);
 					if (b <= a)
-						break;				
-				}	
+						break;
+				}
 			}
 		}
 		return b;
