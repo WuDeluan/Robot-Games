a4ba1a159a595117ed96899ab6268b6b5d58b2db
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2018-08-27 22:03:58 +0800

    Update test.cpp

--- New_Gobang/test.cpp
+++ New_Gobang/test.cpp
@@ -21,8 +21,8 @@ int main()
 	Go.Gobang::setBoard(6, F, WHITE);
 	Go.Gobang::setBoard(6, G, WHITE);
 	Go.Gobang::Print_Checkerboard();
-	Go.minmaxSearch(3, tx, ty);  //大约半小时
-	Go.Gobang::setBoard(tx, ty, COM);
+	Go.minmaxSearch(3);  //大约半小时
+	Go.Gobang::setBoard(X, Y, COM);
 	Go.Gobang::Print_Checkerboard();*/
 
 	/*
@@ -53,7 +53,56 @@ int main()
 	}
 	if (Go.IsLegal(15 - x, y - 'A'))
 		cout << "illegal" << endl;*/
-	
+
+	/*
+	//zobrist测试
+	Go.Gobang::Print_Checkerboard();cout << ZobristKey << endl;
+	Go.Gobang::setBoard(7, H, BLACK);
+	Go.Gobang::Print_Checkerboard(); cout << ZobristKey << endl;
+	Go.Gobang::setBoard(6, F, WHITE);
+	Go.Gobang::Print_Checkerboard(); cout << ZobristKey << endl;
+	Go.Gobang::setEmpty(7, H);
+	Go.Gobang::Print_Checkerboard(); cout << ZobristKey << endl;
+	Go.Gobang::setBoard(7, H, BLACK);Go.Gobang::Print_Checkerboard(); 
+	cout << ZobristKey << endl;
+	Go.Gobang::setBoard(6, G, WHITE);
+	Go.Gobang::Print_Checkerboard(); cout << ZobristKey << endl;*/
+
+	/*
+	COM = BLACK; MAN = WHITE;
+	Go.Gobang::setBoard(7, H, BLACK);
+	Go.Gobang::setBoard(6, H, WHITE);
+	Go.Gobang::setBoard(9, G, BLACK);
+	Go.Gobang::Print_Checkerboard();
+	while (1)
+	{
+		while (true) {
+			cout << "请白方落子：";
+			cin >> x; cin >> y;
+			if (Go.Gobang::setBoard(15 - x, y - 'A', WHITE) == -1)
+				cout << "请选择正确的位置落子！" << endl;
+			else
+				break;
+		}
+		Go.Gobang::Print_Checkerboard();
+		if (Go.Gobang::IsWin(15 - x, y - 'A') == 0)
+			cout << "白方获胜!" << endl;
+		tx = 15 - x; ty = y - 'A';
+
+		if (Go.PreJudge(tx, ty) != 0)
+		{
+			Go.Gobang::setBoard(tx, ty, BLACK);
+			Go.Gobang::Print_Checkerboard();
+		}
+
+		Go.NegaScoutSearch(3, -INFINITY, +INFINITY, COM);
+		Go.Gobang::setBoard(X, Y, BLACK);
+		cout << "黑方落子： (" << 15 - X << " , " << char(Y + 'A') << ")" << endl;
+		Go.Gobang::Print_Checkerboard();
+		if (Go.Gobang::IsWin(15 - X, Y - 'A') == 0)
+			cout << "黑方获胜!" << endl;
+	}*/
+
 	
 	//各搜索算法测试
 	MAN = WHITE;
@@ -63,13 +112,15 @@ int main()
 	Go.Gobang::setBoard(5, G, BLACK);
 	Go.Gobang::setBoard(6, E, WHITE);
 	Go.Gobang::setBoard(6, F, WHITE);
-	Go.Gobang::setBoard(6, G, WHITE); 
+	Go.Gobang::setBoard(6, G, WHITE);
 	//Go.Gobang::setBoard(0, 0, WHITE);
 	//Go.Gobang::setBoard(6, H, BLACK);
 	Go.Gobang::Print_Checkerboard();
-	Go.NegaScoutSearch(tx, ty, 3, -INFINITY, +INFINITY, COM);//缩小搜索范围从1-11，耗时一个半
-	Go.Gobang::setBoard(tx, ty, COM);
+	Go.NegaScoutSearch(4, -INFINITY, +INFINITY, COM);
+	//Go.NegaScoutSearch(3, -INFINITY, +INFINITY, COM);//缩小搜索范围从1-11，耗时一个半
+	Go.Gobang::setBoard(X, Y, COM);
 	Go.Gobang::Print_Checkerboard();
+
 
 	Go.Gobang::Print_Checkerboard();
 	Go.Opening();
